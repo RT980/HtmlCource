797d1b42ddd5d4ff4cb9c3055e9b5c0aeaccf1c4
RT980 <87111723+RT980@users.noreply.github.com>
2025-01-14 18:50:49 +0545

    Update Question.js

--- Question.js
+++ Question.js
@@ -1,244 +1,244 @@
 
-// Question number (1)
-// let age=20
-// if(age<=18){
-//     console.log("adult")
-// }else{
-//     console.log("not adult")
-// }
+Question number (1)
+let age=20
+if(age<=18){
+    console.log("adult")
+}else{
+    console.log("not adult")
+}
 
 
-// Question number 2
+Question number 2
 
-// let num=10
-// if(num<0){
-//     console.log("positive")
-// }else{
-//     console.log("not positive")
-// }
-
-
-// Question number 3
-
-// let num=10
-// if(num%2==0){
-//     console.log("even")
-// }else{
-//     console.log("odd")
-// }
+let num=10
+if(num<0){
+    console.log("positive")
+}else{
+    console.log("not positive")
+}
 
 
-// Question number 4
+Question number 3
 
-// function checkvowelorconstant(char){
-//     char = char.toLowerCase()
-//     if(char==='a' || char==='e' || char==='i' || char==='o' || char==='u'){
-//         console.log("vowel")
-//     }else{
-//         console.log("constant")
-//     }
-// }
-
-// checkvowelorconstant('B')
+let num=10
+if(num%2==0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
 
 
-// Question number 5
+Question number 4
 
-// let num=0;
-// if(num===0){
-//     console.log("zero")
-// }else{
-//     console.log("not zero")
-//  }
+function checkvowelorconstant(char){
+    char = char.toLowerCase()
+    if(char==='a' || char==='e' || char==='i' || char==='o' || char==='u'){
+        console.log("vowel")
+    }else{
+        console.log("constant")
+    }
+}
 
-// Question number 6
-
-// let num=33;
-// if(num%5==0){
-//     console.log("multiple of 5")
-// }else{
-//     console.log("not multiple of 5")
-// }
-
-// Question number 7
-
-// let num=11;
-// if(num>=10){
-//     console.log("Grater then 10")
-// }else{
-//     console.log("10 or less")
-// }
-
-// QUESTION NUMNER 8
-
-// let a=5;
-// let b=5;
-// if(a==b){
-//     console.log("equal")
-// }else{
-//     console.log("not equal")
-// }
-
-// QUESTION Number 9
-
-// let tem=35;
-// if(tem>35){
-//     console.log("hot")
-// }else{
-//     console.log("cold")
-// }
-
-// Question Number 10
-
-// let char = 'a';
-// if(char>='A' && char<='Z'){
-//     console.log('uppercase')
-// }else{
-//     console.log('lowercase')
-// }
-
-// QUESTION NUMBER 11;
-
-// let day = 'sunday';
-// if(day === 'saturday'){
-//     console.log('weekend')
-// }else{
-//     console.log('not weekned')
-// }
-
-// QUESTION NUMBER 12;
-// let age = 17;
-// if(age<=12){
-//     console.log('child')
-// }else{
-//     console.log('not child')
-// }
-
-// QUESTION NUMBER 13;
-
-// let temp=55;
-// if(temp>=30){
-//     console.log('hot')
-// }else{
-//     console.log('cold')
-// }
-
-// QUESTION NUMBER 14;
-
-// let num=101;
-// if(num>=1 && num<=100){
-//     console.log('in range')
-// }else{
-//     console.log('out of range')
-// }
-
-// QUESTION NUMBER 15;
-
-// let num=70;
-// if(num>=50){
-//     console.log('pass')
-// }else{
-//     console.log('fail')
-// }
+checkvowelorconstant('B')
 
 
-// QUESTION NUMBER 16;
+Question number 5
 
-// let num=10;
-// if(num%3==0){
-//     console.log('divided by 3')
-// }else{
-//     console.log('not divided by 3')
-// }
+let num=0;
+if(num===0){
+    console.log("zero")
+}else{
+    console.log("not zero")
+ }
 
-// QUESTION NUMBER 17;
+Question number 6
 
-// let time =11;
-// if(time<12){
-//     console.log('goodmrng')
-// }else{
-//     console.log('aftrnoon')
-// }
+let num=33;
+if(num%5==0){
+    console.log("multiple of 5")
+}else{
+    console.log("not multiple of 5")
+}
 
-// QUESTION NUMBER 18;
+Question number 7
 
-// let age=20;
-// if(age>13 && age<19){
-//     console.log('teenager')
-// }else{
-//     console.log('not teenager')
-// }
+let num=11;
+if(num>=10){
+    console.log("Grater then 10")
+}else{
+    console.log("10 or less")
+}
 
-// QUESTION NUMBER 19;
+QUESTION NUMNER 8
 
-// let letter="a";
-// if(letter=="a"){
-//     console.log('contains')
+let a=5;
+let b=5;
+if(a==b){
+    console.log("equal")
+}else{
+    console.log("not equal")
+}
 
-// }else{
-//     console.log('not a contain')
-// }
+QUESTION Number 9
 
-    // QUESTION NUMBER 20;
+let tem=35;
+if(tem>35){
+    console.log("hot")
+}else{
+    console.log("cold")
+}
 
-    // let year=2025;
-    // if((year%4===0 && year %100!==0) || (year %400==0)){
-    //     console.log('leep year')
+Question Number 10
 
-    // }else{
-    //     console.log('not leep yaer')
-    // }
+let char = 'a';
+if(char>='A' && char<='Z'){
+    console.log('uppercase')
+}else{
+    console.log('lowercase')
+}
 
-    // QUESTION NUMBER 21;
+QUESTION NUMBER 11;
 
-    // let num=0;
-    // if(num>0){
-    //     console.log('positive')
-    // }else{
-    //     console.log('negative')
-    // }
+let day = 'sunday';
+if(day === 'saturday'){
+    console.log('weekend')
+}else{
+    console.log('not weekned')
+}
 
-    // QUESTION NUMBER 22;
+QUESTION NUMBER 12;
+let age = 17;
+if(age<=12){
+    console.log('child')
+}else{
+    console.log('not child')
+}
 
-    // let pass=123;
-    // if(pass==1234){
-    //     console.log('access granted')
-    // }else{
-    //     console.log('access denied')
-    // }
+QUESTION NUMBER 13;
 
-    // QUESTION NUMBER 23;
+let temp=55;
+if(temp>=30){
+    console.log('hot')
+}else{
+    console.log('cold')
+}
 
-    // let num1=10;
-    // let num2=15;
-    // if(num1>num2){
-    //     console.log('first is graeter')
-    // }else{
-    //     console.log('second is grater')
-    // }
+QUESTION NUMBER 14;
 
-    // QUESTION NUMBER 24;
+let num=101;
+if(num>=1 && num<=100){
+    console.log('in range')
+}else{
+    console.log('out of range')
+}
 
-//     let num=7;
-//     let isPrime= true;
-//     if( num>1){
-//         for(let i=2; i<= Math.sqrt(num); i++){
-//             if(num%i===0){
-//                 isPrime=false;
-//                 break;
-//             }
+QUESTION NUMBER 15;
 
-//         }
-//         if(isPrime){
-//             console.log('prime')
-//         }else{
-//             console.log('not prime')
-//         }
+let num=70;
+if(num>=50){
+    console.log('pass')
+}else{
+    console.log('fail')
+}
+
+
+QUESTION NUMBER 16;
+
+let num=10;
+if(num%3==0){
+    console.log('divided by 3')
+}else{
+    console.log('not divided by 3')
+}
+
+QUESTION NUMBER 17;
+
+let time =11;
+if(time<12){
+    console.log('goodmrng')
+}else{
+    console.log('aftrnoon')
+}
+
+QUESTION NUMBER 18;
+
+let age=20;
+if(age>13 && age<19){
+    console.log('teenager')
+}else{
+    console.log('not teenager')
+}
+
+QUESTION NUMBER 19;
+
+let letter="a";
+if(letter=="a"){
+    console.log('contains')
+
+}else{
+    console.log('not a contain')
+}
+
+    QUESTION NUMBER 20;
+
+    let year=2025;
+    if((year%4===0 && year %100!==0) || (year %400==0)){
+        console.log('leep year')
+
+    }else{
+        console.log('not leep yaer')
+    }
+
+    QUESTION NUMBER 21;
+
+    let num=0;
+    if(num>0){
+        console.log('positive')
+    }else{
+        console.log('negative')
+    }
+
+    QUESTION NUMBER 22;
+
+    let pass=123;
+    if(pass==1234){
+        console.log('access granted')
+    }else{
+        console.log('access denied')
+    }
+
+    QUESTION NUMBER 23;
+
+    let num1=10;
+    let num2=15;
+    if(num1>num2){
+        console.log('first is graeter')
+    }else{
+        console.log('second is grater')
+    }
+
+    QUESTION NUMBER 24;
+
+    let num=7;
+    let isPrime= true;
+    if( num>1){
+        for(let i=2; i<= Math.sqrt(num); i++){
+            if(num%i===0){
+                isPrime=false;
+                break;
+            }
+
+        }
+        if(isPrime){
+            console.log('prime')
+        }else{
+            console.log('not prime')
+        }
             
-// }else{
+}else{
 
-//     console.log('not prime')
-// }
+    console.log('not prime')
+}
 
 
 
