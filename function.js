a9f68fe41c67b2bc8e0eb8a840763ad1a0179452
RT980 <87111723+RT980@users.noreply.github.com>
2025-01-14 18:48:25 +0545

    added js

---
+++ function.js
@@ -0,0 +1,187 @@
+// QUESTION NUMBER 1;
+
+// function number(){
+//     for(i=1; i<=10; i++){
+//         console.log(i)
+//     }
+// }
+// number()
+
+
+// QUESTION NUMBER 2;
+
+// function SumOfNumber(n){
+//     let sum=0;
+//     for(i=1; i<=n; i++){
+//         sum = sum +i
+//     }
+
+//     return sum;
+
+// }
+
+// let n = 5;
+// console.log( n, SumOfNumber(n) )
+
+
+// QUESTION NUMBER 3;
+
+// function EvenNumber(){
+//     for(i=1; i<=20; i++){
+//         if(i%2===0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// EvenNumber()
+
+
+// QUESTION NUMBER 4;
+
+// function OddNumber(){
+//     for(i=1; i<=20; i++){
+//         if(i%2 !==0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// OddNumber()
+
+// QUESTION NUMBER 5;
+
+// function factorial(n){
+//     let result = 1;
+//     for(i=1; i<=n; i++){
+//         result = result * i;
+//     }
+
+//     return result;
+// }
+
+// let n=9;
+// console.log('facteriol of given number', n, 'is', factorial(n))
+
+// QUESTION NUMBER 6;
+
+// function MultipleOfFive(){
+//     for(i=1; i<=10; i++){
+//         console.log(`5*${i} = ${5*i}`)
+//     }
+// }
+
+// MultipleOfFive()
+
+
+// QUESTION NUMBER 7;
+
+// function ReverseOrder(){
+//     for(i=10; i>=1; i--){
+//         console.log(i)
+//     }
+// }
+// ReverseOrder()
+
+// QUESTION NUMBER 8;
+// function MultipleOfThree(){
+//     for(i=1; i<=30; i++){
+//         if(i%3===0){
+//             console.log(i)
+//         }
+//     }
+// }
+// MultipleOfThree()
+
+// QUESTION NUMBER 9;
+
+// function SumOfEvenNumber(){
+//     let sum = 0;
+//     for(i=2; i<=50; i ++){
+//         if(i%2===0){
+//             sum += i;
+            
+//         }
+     
+//     }
+//     return sum;
+// }
+// console.log('sum of even numbe between 1 to 50',',', SumOfEvenNumber())
+
+// QUESTION NUMBER 10;
+
+// function SumOfOddNumber(){
+//     let sum =0;
+//     for(i=1; i<=50; i++){
+//         if(i%2 !==0){
+//             sum = sum + i;
+//         }
+//     }
+//     return sum;
+
+// }
+
+// console.log('sum of odd number between 1 to 50', ',', SumOfOddNumber())
+
+// QUESTION NUMBER 11;
+
+// function MultipleOfFive(){
+//     for(i=1; i<=100; i++){
+//         if(i%5==0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// MultipleOfFive()
+
+// QUESTION NUMBER 12;
+
+// function Fibonacci() {
+//     let a = 0;
+//     let b = 1;
+//     console.log(a);
+//     console.log(b);
+//         let count = 0;
+//     for (i = 3; i <=10; i++) {
+//         let next = a + b;
+//         console.log(next);
+//         a = b;
+//         b = next;
+//         count++
+//         if(count === 10){
+            
+//         }
+        
+//     }
+// }
+
+// Fibonacci();
+
+// QUESTION NUMBER 14;
+
+// function SquareNumber(){
+//     for(i=1; i<=10; i++){
+//         console.log(`${i}=${i*i}`)
+//     }
+
+// }
+
+// SquareNumber()
+
+// QUESTION NUMBER 15;
+
+// function SumOfSquareNumber(n){
+//     let num=1;
+//     for(i=2;i<=num;i ++){
+//         console.log(`${i}=${i*i}`)
+//             num = num + i;
+
+    
+
+//     return num;
+// }
+// }
+// let n=2;
+
+// console.log( SumOfSquareNumber(n))
---
+++ loop.js
@@ -0,0 +1,167 @@
+// Question number 1
+
+// for(i=1; i<=20; i++){
+//     console.log(i)
+// }
+
+// QUESTION NUMBER 2
+
+// let sum=0;
+// for(i=1; i<=10; i++){
+//         sum +=  i
+//     }
+
+// console.log( sum)
+
+
+// QUESTION NUMBER 3
+
+// for(i=1; i<=10; i++){
+//     console.log(`5*${i}=${5*i}`)
+// }
+
+// QUESTION NUMBER 4
+
+// for(i=1; i<=100; i++){
+//     console.log(i)
+// }
+
+// QUESTION NUMBER 5
+
+// let sum=0;
+// for(i=1; i<=10; i++){
+//     if(i%2 !==0){
+//         sum = sum + i
+
+//     }
+// }
+// console.log(sum)
+
+// QUESTION NUMBER 6
+// for(i=20; i>=1; i--){
+//     console.log(i)
+
+// }
+
+// QUESTION NUMBER 7
+// for(i=10; i>=1; i--){
+//     console.log(`3*${i}= ${3*i}`)
+// }
+
+// QUESTION NUMBER 8
+
+// for(i=1; i<=10; i++){
+//     console.log(`${i}= ${i*i}`)
+// }
+
+// QUESTION NUMBER 9
+// let sum=0;
+// for(i=1; i<=100; i++){
+//     if(i%2==0){
+//         sum = sum + i
+//     }
+// }
+// console.log(sum)
+
+// QUESTION NUMBER 10
+
+// for(i=1; i<=5; i++){
+//     console.log(`${i}=${i*i*i}`)
+// }
+
+// QUESTION NUMBER 11
+
+// for(i=50; i>=1; i--){
+//     console.log(i)
+// }
+
+// QUESTION NUMBER 12
+
+// let sum=0;
+// for(i=1; i<=19; i++){
+//     if(i%2 !==0){
+//         sum = sum + i;
+//     }
+// }
+// console.log(sum)
+
+// QUESTION NUMBER 13
+
+// let sum = 0;
+// for(i=1; i<=9; i+=2){
+//     sum += i
+// }
+// console.log(sum)
+
+// QUESTION NUMBER 14
+
+// for(i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// QUESTION NUMBER 15
+//  for(i=1; i<=10; i++){
+//     console.log(`2^${i}=${2**i}`)
+//  }
+
+// QUESTION NUMBER 16
+
+// let sum=0;
+// for(i=1; i<=20; i++){
+//     if(i%2==0){
+//         sum = sum +i
+//     }
+// }
+// console.log(sum)
+
+// QUESTION NUMBER 17
+
+// for(i=10; i>=1; i--){
+//     console.log(`4*${i}=${4*i}`)
+// }
+
+// QUESTION NUMBER 19
+// for(i=30; i>=10; i-=2){
+//     console.log(i)
+
+
+// }
+
+
+// QUESTION NUMBER 18 
+
+// let count= 1 ;
+// for(count<=12;count++;){
+//     let isPrime=true;
+
+//     for(let i=2; i< Math.sqrt(count); i++){
+//         if(count%i==0){
+//             isPrime=false;
+//             count++
+//             break;
+
+//         }
+//     }
+//     if(isPrime){
+//         console.log(count)
+//     }
+
+// }
+// let num = 11;
+// let isPrime=true;
+
+// for(let i=2; i<num; i++){
+//     if(num %i==0){
+//         isPrime=false;
+//         break;
+
+//     }
+
+// }
+
+// if(isPrime){
+//     console.log(`${num} is a prime number`);
+// }else{
+//     console.log(`${num} is not prime number`);
+// }
+ 
